5a9d0e7e260fc1650cd220037d682c0bbb853a54
Tim Besard <tim.besard@gmail.com>
2018-06-08 09:52:32 +0200

    Silence build output.

--- images/build.sh
+++ images/build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 
 for ver in base/*; do
-    docker build --no-cache $ver --tag base/julia:$(basename $ver)
+    docker build --quiet --no-cache $ver --tag base/julia:$(basename $ver)
 done
 
 for ver in derived/*; do
-    docker build $ver --tag juliagpu/julia:$(basename $ver)
+    docker build --quiet $ver --tag juliagpu/julia:$(basename $ver)
 done
