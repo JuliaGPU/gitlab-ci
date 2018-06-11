67bf15d5c6c6d5086d7fa911b705c1956b05b63d
Tim Besard <tim.besard@gmail.com>
2018-06-11 09:10:47 +0200

    Allow choosing images to rebuild.

--- images/build.sh
+++ images/build.sh
@@ -1,9 +1,22 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-for ver in base/*; do
-    docker build --quiet --no-cache $ver --tag base/julia:$(basename $ver)
+# either rebuild all base images, or let the user specify
+if [[ "$#" -eq 0 ]]; then
+    echo "no args"
+    IMAGES=$(ls base/*)
+    IMAGES=()
+    for base in base/*; do
+        IMAGES+=($(basename $base))
+    done
+else
+    IMAGES=("$@")
+fi
+
+for base in ${IMAGES[@]}; do
+    docker build --quiet --no-cache base/$base --tag base/julia:$base
+
+    for derived in derived/${base}*; do
+        docker build --quiet $derived --tag juliagpu/julia:$(basename $derived)
+    done
 done
 
-for ver in derived/*; do
-    docker build --quiet $ver --tag juliagpu/julia:$(basename $ver)
-done
